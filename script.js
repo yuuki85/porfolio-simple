c9b205bacb00cc65f89d9fb07b7fcc2f405072bb
Syahmi Adlan <113775946+yuuki85@users.noreply.github.com>
2023-11-21 18:37:33 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Add any JavaScript functionality here
+    // For example, you can handle form submission or add animations.
+    
+    // Example: Form submission handling
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        alert('Form submitted!');
+    });
+});
